6167b4d9ea233876d81759d0a2b0def78d646194
acer <acer32@acer.com>
2016-12-08 20:05:51 +0800

    add one println into GitTest.java

--- GitTest.java
+++ GitTest.java
@@ -2,8 +2,9 @@ public class GitTest{
 	
 	public static void main(String args[]){
 		
-		System.out.println("这是一个测试");
+		System.out.println("这是第一个测试");
 		System.out.println("这是第二个测试");
+		System.out.println("这是第三个测试");
 	}
 	
 }
