a8b6912e3faf6aa212a27312430ea7a936730bf0
acer <acer32@acer.com>
2016-12-08 20:44:09 +0800

    created GitTest2.java;add a line at last into GitTest.java

--- GitTest.java
+++ GitTest.java
@@ -5,6 +5,7 @@ public class GitTest{
 		System.out.println("这是第一个测试");
 		System.out.println("这是第二个测试");
 		System.out.println("这是第三个测试");
+		System.out.println("这是第四个测试");
 	}
 	
 }
---
+++ GitTest2.java
@@ -0,0 +1,8 @@
+public class GitTest2{
+	
+	public static void main(String args[]){
+		
+		System.out.println("这是第一个测试");
+	}
+	
+}
